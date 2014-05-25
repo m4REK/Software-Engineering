405e3b8d406039e37d521edec810bee8c9c0db94
m4REK <vukadinovic.m@live.com>
2014-05-25 17:21:51 +0200

    CopyEncryptor.java geändert

--- Encryptor/CopyEncryptor.java
+++ Encryptor/CopyEncryptor.java
@@ -1,8 +1,9 @@
 public class CopyEncryptor implements Encryptor {
-	String text;
+	String text = "";
+	int key = 0;
 
 	@Override
-	public String setEncryptStrategie(String text) {
+	public String setEncryptStrategie(String text, int key) {
 		return text;
 
 	}
